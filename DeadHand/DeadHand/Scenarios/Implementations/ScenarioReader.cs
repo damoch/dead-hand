ab76e518766b3e8e3182c4cea0bd9a2e575a6149
Damian Ochramowicz <damochramowicz260@gmail.com>
2022-12-29 18:16:51 +0100

    first stage of scenario refactor

---
+++ DeadHand/DeadHand/Scenarios/Implementations/ScenarioReader.cs
@@ -0,0 +1,34 @@
+﻿using DeadHand.Commands.Implementations;
+using DeadHand.Scenarios.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadHand.Scenarios.Implementations
+{
+    internal class ScenarioReader : ScenarioBase
+    {
+        public ScenarioReader(EmailCommand emailService, CheckRadioCommand radioService) : base(emailService, radioService)
+        {
+            //TODO: Implement
+        }
+        public override string ScenarioName => throw new NotImplementedException();
+
+        public override void ScenarioEndingLaunch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ScenarioEndingShutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartScenario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
